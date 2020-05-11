a27184b50bf1b335831bc5dd63cb63ad110d8813
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-05-11 16:36:58 +0200

    Stub out master branch (use as template repository) Sort Gradle dependencies Version bump Crop ktor.png

--- src/main/kotlin/com/jetbrains/handson/website/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/website/Application.kt
@@ -1,10 +1,13 @@
 package com.jetbrains.handson.website
 
 import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
 import io.ktor.application.*
 import io.ktor.freemarker.*
 import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.*
+import io.ktor.request.receiveParameters
 import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.html.*
@@ -13,41 +16,5 @@ import kotlinx.html.*
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    install(FreeMarker) {
-        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
-    }
-    routing {
-        static("/static") {
-            resources("files")
-        }
-        get("/html") {
-            call.respondHtml {
-                head {
-                    title {
-                        +"Returning HTML using Kotlinx.HTML"
-                    }
-                }
-                body {
-                    h1 {
-                        +"Kotlinx.Html"
-                    }
-                    p {
-                        +"We're using a static HTML DSL"
-                    }
-                    val numbers = 1..10
-                    ul {
-                        numbers.forEach {
-                            li {
-                                value = it.toString()
-                                +"Item $it"
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        get("/freemarker") {
-            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
-        }
-    }
+
 }
--- src/main/kotlin/com/jetbrains/handson/website/IndexData.kt
+++
@@ -1,3 +0,0 @@
-package com.jetbrains.handson.website
-
-data class IndexData(val items: List<Int>)
