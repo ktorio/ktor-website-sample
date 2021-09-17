23f8827ac78969eff994397cc8a81d5748d5c27b
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-09-17 12:27:49 +0300

    update to 2.0.0

--- src/main/kotlin/com/jetbrains/handson/website/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/website/Application.kt
@@ -2,14 +2,14 @@ package com.jetbrains.handson.website
 
 import freemarker.cache.*
 import freemarker.core.HTMLOutputFormat
-import io.ktor.application.*
-import io.ktor.freemarker.*
-import io.ktor.html.respondHtml
+import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.html.respondHtml
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.content.*
-import io.ktor.request.receiveParameters
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.http.content.*
+import io.ktor.server.request.receiveParameters
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.html.*
 
 
